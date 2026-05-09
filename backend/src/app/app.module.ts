8205ce4ec891b76d04884d7b5699fa47fc6322f2
Darya Firzina <darya.firzina@mail.ru>
2026-05-10 02:14:48 +0500

    feat: add prisma setup for backend

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      expandVariables: true,
-      isGlobal: true,
-      //   ignoreEnvFile: !IS_DEV_ENV,
-    }),
-  ],
-  controllers: [],
-  providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			expandVariables: true,
+			isGlobal: true
+			//   ignoreEnvFile: !IS_DEV_ENV,
+		}),
+		PrismaModule
+	],
+	controllers: [],
+	providers: []
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+	const app = await NestFactory.create(AppModule);
+
+	const config = app.get(ConfigService);
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN')
+	});
+
+	await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
 
 bootstrap();
